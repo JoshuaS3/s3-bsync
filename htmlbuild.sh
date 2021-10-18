5e138005a32eac7fbc8979b39a94ca4f80c1e672
Josh Stockin <josh@joshstock.in>
2021-10-18 11:34:57 -0500

    Remove README HTML tools

--- htmlbuild.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# use md2html with basic styling to test README.md
-cat <(echo "<style>body{max-width:650px;margin:20px auto;color:#252525}</style>") <(md2html README.md) > README.html
